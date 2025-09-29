4d66c88c893edf0f8e78b1efec1a4d8503cd81db
jawwad183 <mjwdmufti@gmail.com>
2025-09-29 21:19:11 +0300

    testing auto grader

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function studentCard(props){
+    return(
+        <div>
+            <h3>Name: {props.name}</h3>
+            <p>Student ID: {props.id}</p>
+            <p>Department: {props.dept}</p>
+        </div>
+    )
+}
+
+export default studentCard;
